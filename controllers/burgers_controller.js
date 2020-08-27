e5667d433dc6e587a94707d29eff5264fcf0a3bd
Danny Choi <choi.danny20@gmail.com>
2020-08-27 14:11:33 -0500

    fixed error that was causing 404 for my PUT request

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -10,22 +10,11 @@ router.get("/", function (req, res) {
   });
 });
 
-router.post("/api/burgers", function (req, res) {
-  burger.insertOne(
-    ["burger_name", "devoured"],
-    [req.body.burger_name, req.body.devoured],
-    function (result) {
-      // Send back the ID of the new quote
-      res.json({ id: result.insertId });
-    }
-  );
-});
-
-router.put("/api/burger/:id", function (req, res) {
+router.put("/api/burgers/:id", function (req, res) {
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
-
+  console.log(req.body);
   burger.updateOne(
     {
       devoured: req.body.devoured,
@@ -41,5 +30,22 @@ router.put("/api/burger/:id", function (req, res) {
     }
   );
 });
+
+router.post("/api/burgers", function (req, res) {
+  burger.insertOne(
+    ["burger_name", "devoured"],
+    [req.body.burger_name, req.body.devoured],
+    function (result) {
+      // Send back the ID of the new quote
+      res.json({ id: result.insertId });
+    }
+  );
+});
+
+// router.put("/", function (req, res){
+
+// res.json({ok:true})
+
+// })
 
 module.exports = router;
