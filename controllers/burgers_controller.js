8c8b596f735687d46a0ccb7ed19cf55a5fda9e9f
Danny Choi <choi.danny20@gmail.com>
2020-08-26 23:26:02 -0500

    added function when submitted adds the item to page. Also changed to the correct ajax URL for POST methd

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -10,8 +10,8 @@ router.get("/", function (req, res) {
   });
 });
 
-router.post("/api/burgers/:id", function (req, res) {
-  burger.create(
+router.post("/api/burgers", function (req, res) {
+  burger.insertOne(
     ["burger_name", "devoured"],
     [req.body.burger_name, req.body.devoured],
     function (result) {
@@ -21,6 +21,25 @@ router.post("/api/burgers/:id", function (req, res) {
   );
 });
 
-// router.put("/api/burgers/:id", function (req, res) {});
+router.put("/api/burger/:id", function (req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burger.updateOne(
+    {
+      devoured: req.body.devoured,
+    },
+    condition,
+    function (result) {
+      if (result.changedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    }
+  );
+});
 
 module.exports = router;
--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -1,39 +1,36 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function () {
-  //   $(".change-status").on("click", function (event) {
-  //     let id = $(this).data("id");
-  //     let isDevoured = $(this).data("devoured");
-  //     console.log(isDevoured);
-  // let newSleepState = {
-  //   sleepy: newSleep,
-  // };
+  $(".change-status").on("click", function (event) {
+    let id = $(this).data("id");
 
-  // // Send the PUT request.
-  // $.ajax("/api/cats/" + id, {
-  //   type: "PUT",
-  //   data: newSleepState,
-  // }).then(function () {
-  //   console.log("changed sleep to", newSleep);
-  //   // Reload the page to get the updated list
-  //   location.reload();
-  // });
-  //   });
+    let newBurgerState = {
+      devoured: true,
+    };
+
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newBurgerState,
+    }).then(function () {
+      location.reload();
+    });
+  });
 
   $(".create-form").on("submit", function (event) {
-    // Make sure to preventDefault on a submit event.
     event.preventDefault();
 
-    var newBurger = {
+    let newBurger = {
       burger_name: $("#ca").val().trim(),
       devoured: $("[name=devoured]:checked").val().trim(),
     };
 
-    // Send the POST request.
+    console.log(newBurger);
+
     $.ajax("/api/burgers", {
       type: "POST",
       data: newBurger,
     }).then(function () {
-      // Reload the page to get the updated list
+      console.log("new burger");
+
       location.reload();
     });
   });
