13d117ec60939048fa16c8042ddb075f08a75427
Danny Choi <choi.danny20@gmail.com>
2020-09-05 21:53:39 -0500

    working on configuration for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,9 @@
 const mysql = require("mysql");
-
+let connection;
 if (process.env.JAWSDB_URL) {
-  const connection = mysql.createConnection(process.env.JAWDB_URL);
+  connection = mysql.createConnection(process.env.JAWDB_URL);
 } else {
-  const connection = mysql.createConnection({
+  connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
